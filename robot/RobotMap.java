76249c992357d040d7f6e7e7f52a9012c8ff7728
NutsAndVolts7725 <60159683+NutsAndVolts7725@users.noreply.github.com>
2020-01-21 17:52:48 -0600

    Add files via upload

---
+++ robot/RobotMap.java
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+public class RobotMap {
+
+  //Motors
+	public static final int MOTOR_INTAKE_1_ID = 5; //Entrada
+  public static final int MOTOR_INTAKE_2_ID = 6; //Medio
+  public static final int MOTOR_INTAKE_3_ID = 7; //Disparador
+  
+  //Controllers
+  public static final int DRIVER_CONTROLLER = 0;
+  
+  //Buttons
+	public static final int JOYSTICK_1_BUTTON_11 = 11;
+  public static final int JOYSTICK_1_BUTTON_12 = 12;
+  public static final int JOYSTICK_1_BUTTON_9 = 9;
+  
+  //Driver limitations and values
+  public static final double GLOBAL_VELOCITY = 0.5;
+  
+  
+
+
+}
